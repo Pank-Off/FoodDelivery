cb5ec8857e1e4475b58828fa5392b0a1264cc554
Punk_Off <pankov.kirill1997@yandex.ru>
2022-02-15 17:12:56 +0300

    rename packages and files

--- app/src/main/java/ru/punkoff/fooddelivery/ui/cart/CartFragment.kt
+++ app/src/main/java/ru/punkoff/fooddelivery/ui/cart/CartFragment.kt
@@ -1,4 +1,4 @@
-package ru.punkoff.fooddelivery.ui.notifications
+package ru.punkoff.fooddelivery.ui.cart
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import ru.punkoff.fooddelivery.databinding.FragmentNotificationsBinding
+import ru.punkoff.fooddelivery.databinding.FragmentCartBinding
 
-class NotificationsFragment : Fragment() {
+class CartFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentCartBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,9 +23,9 @@ class NotificationsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+            ViewModelProvider(this).get(CartViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentCartBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textNotifications
--- app/src/main/java/ru/punkoff/fooddelivery/ui/cart/CartViewModel.kt
+++ app/src/main/java/ru/punkoff/fooddelivery/ui/cart/CartViewModel.kt
@@ -1,13 +1,13 @@
-package ru.punkoff.fooddelivery.ui.home
+package ru.punkoff.fooddelivery.ui.cart
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class CartViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is notifications Fragment"
     }
     val text: LiveData<String> = _text
 }
